2bca591c1951c925106e1f3aeca330b4825fc666
Angello <arios@veritran.com>
2024-08-23 09:40:55 -0500

    fix firebase

--- src/infraestructure/firebase.ts
+++ src/infraestructure/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc, addDoc, getDocs } from "firebase/firestore"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCr5PLi-dUdnZWKl6ILwWHFPsQzJEBIun4",
-    authDomain: "cv-angello.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     projectId: "cv-angello",
     storageBucket: "cv-angello.appspot.com",
-    messagingSenderId: "244403857618",
-    appId: "1:244403857618:web:721a9f7118a67c6ed2e50b",
-    measurementId: "G-WX7TZTVBQ3"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 const app = initializeApp(firebaseConfig);
